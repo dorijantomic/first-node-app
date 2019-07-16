e19592b95ea8af9be2a062aa729b92eb434c30f2
[Dorijan] <[dorijan11@gmail.com]>
2019-07-16 18:19:50 +0200

    Created a server which sends responses after a request

--- app.js
+++ app.js
@@ -7,7 +7,14 @@
 
 */
  const server = http.createServer((req,res) => { 
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</title> </head>')
+    res.write('<body><h1>Hello from my first Node.js server WOOHOO!!!!!</h1></body>')
+    res.write('</header>')
+    res.write('</html>')
+    res.end();
  }) 
 
 // The server that we've made above requires a method to be callede on it which tells it to listen for users that enter localhost(3000) <-- we define the port
