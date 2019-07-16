b3714f3bed089ac658c583d00ecfac34731d8771
[Dorijan] <[dorijan11@gmail.com]>
2019-07-17 00:50:08 +0200

    added a response that stores chunks of data and then places them in the buffer zone to be worked on

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  const http = require('http');
-
+const fs = require('fs');
 
 /*
  The createServer method allows us to create a server, it requires a request and a response.
@@ -7,14 +7,40 @@
 
 */
  const server = http.createServer((req,res) => { 
-    console.log(req.url, req.method, req.headers)
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>My First Page</title> </head>')
-    res.write('<body><h1>Hello from my first Node.js server WOOHOO!!!!!</h1></body>')
-    res.write('</header>')
+     const url = req.url
+     const method = req.method;
+   if (url === '/') {
+  
+    res.write('<html>') // the header returns a response with the html that we used .write to make
+    res.write('<head><title>Enter Message</title> </head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button> </input> </form></body>')
     res.write('</html>')
-    res.end();
+    return res.end();
+   }
+
+   if(url === '/message' && method === 'POST') {
+       const body = [];
+       req.on('data', (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+       }) 
+       req.on('end', () => { // we use this event listener to work with the chunks of data we gained
+        const parsedBody = Buffer.concat(body).toString() // Creates a new buffer with all the chunks received from the req.on and converts it to string
+        const message = parsedBody.split('=')[1]; // We take the parsed body split it at the equals sign and store the element on the right side of the =
+        fs.writeFileSync('message.txt', message);
+       })
+        
+        res.statusCode= 302;
+        res.setHeader('Location', '/')
+        return res .end()
+   }
+   res.setHeader('Content-Type', 'text/html') // allows us to set headers quite self evident
+   res.write('<html>') // the header returns a response with the html that we used .write to make
+   res.write('<head><title>My First Page</title> </head>')
+   res.write('<body><h1>Hello from my first Node.js server WOOHOO!!!!!</h1></body>')
+   res.write('</header>')
+   res.write('</html>')
+   res.end();
  }) 
 
 // The server that we've made above requires a method to be callede on it which tells it to listen for users that enter localhost(3000) <-- we define the port
